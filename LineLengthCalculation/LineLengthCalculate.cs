6396a38ea72037a130e643575bd8a7f9fc1ae33e
satyamshete <shetesatyam99@gmail.com>
2022-08-11 01:42:19 +0530

    [Satyam] Add: UC1- single Line length calculation

---
+++ LineLengthCalculation/LineLengthCalculate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineLengthCalculation
+{
+    internal class LineLengthCalculate
+    {
+        public void CalculateLength()
+        {
+            Console.WriteLine("Calculate length");
+            int x1, y1, x2, y2;
+            Console.WriteLine("Enter 4 end points of line ");
+            x1 = int.Parse(Console.ReadLine());
+            x2 = int.Parse(Console.ReadLine());
+            y1 = int.Parse(Console.ReadLine());
+            y2 = int.Parse(Console.ReadLine());
+
+
+            float length = (float)Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+
+            Console.WriteLine("Length of line is= " + length);
+        }
+    }
+}
---
+++ LineLengthCalculation/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LineLengthCalculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Line Length Calculator");
+
+            ////UC1
+            LineLengthCalculate lineLengthCalculate = new LineLengthCalculate();
+            lineLengthCalculate.CalculateLength();
+        }
+    }
+}
